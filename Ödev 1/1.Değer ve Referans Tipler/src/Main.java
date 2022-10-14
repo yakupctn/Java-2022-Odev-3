de85b438d90c3038111cfe4c087e6e13c23dd165
YAKUP ÇETİN <104516900+yakupctn@users.noreply.github.com>
2022-10-15 00:59:21 +0300

    Done

---
+++ Ödev 1/1.Değer ve Referans Tipler/src/Main.java
@@ -0,0 +1,24 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		// Değer Tip
+		int sayi1 = 10;
+		int sayi2 = 20;
+		sayi1 = sayi2 ;
+		sayi2 = 100;
+		
+		System.out.println(sayi1);  // => 20
+		
+		// Referans Tip
+		int [] sayilar1 = {1, 2, 3}; 
+		int [] sayilar2 = {10, 20, 30};
+		sayilar1 = sayilar2;
+		
+		sayilar2[0] = 1000;
+		
+		System.out.println(sayilar2[0]);  // => 1000
+
+	}
+
+}
